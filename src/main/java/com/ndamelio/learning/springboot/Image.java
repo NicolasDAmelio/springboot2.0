8d5c3c00d12cb52d5ecce0e0bb5297475587c17c
Nicolas D'Amelio <ndamelio@capitaria.com>
2018-08-07 23:19:25 -0400

    page 63

---
+++ src/main/java/com/ndamelio/learning/springboot/Image.java
@@ -0,0 +1,15 @@
+package com.ndamelio.learning.springboot;
+
+import lombok.Data;
+
+@Data
+public class Image {
+
+    private String id;
+    private String name;
+
+    public Image(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
--- src/main/java/com/ndamelio/learning/springboot/SpringbootApplication.java
+++ src/main/java/com/ndamelio/learning/springboot/SpringbootApplication.java
@@ -2,11 +2,18 @@ package com.ndamelio.learning.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    @Bean
+    HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
     }
 }
